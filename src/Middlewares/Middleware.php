5c6b66c5710e7dafdc9779844f235a4f5e0f1a05
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2023-02-17 09:38:35 +0100

    store parameters

--- src/Middlewares/Middleware.php
+++ src/Middlewares/Middleware.php
@@ -10,7 +10,12 @@ class Middleware implements IMiddleware{
         App::use('amz-sp-api',function(){
             try{
                 if (isset($_GET['amazon_callback_uri'])){
+                    $db = App::get('session')->getDB();
                     $_SESSION['amazon_callback_uri'] = $_GET['amazon_callback_uri'];
+                    $_SESSION['amazon_state'] = $_GET['amazon_state'];
+                    $_SESSION['amazon_version'] = $_GET['version'];
+                    $_SESSION['selling_partner_id'] = $_GET['selling_partner_id'];
+                    $_SESSION['tualo_state_for_amazon'] = $db->singleValue('select uuid() u',[],'u');
                 }
                 if (
                     isset($_SESSION['tualoapplication']) && 
@@ -18,7 +23,41 @@ class Middleware implements IMiddleware{
                     ($_SESSION['tualoapplication']['loggedIn']===true) &&
                     isset($_SESSION['amazon_callback_uri'])
                 ){
-                    $url = $_SESSION['amazon_callback_uri'];
+                    $url = $_SESSION['amazon_callback_uri']
+                    .'&amazon_state='.$_SESSION['amazon_state']
+                    .'&state='.$_SESSION['tualo_state_for_amazon']
+                    .'&version='.$_SESSION['amazon_version'];
+
+                    $db->direct('create table if not exists amazon_seller_partner_api (keyname varchar(50) primary key, `value` varchar(4000) );');
+                    $db->direct('insert into amazon_seller_partner_api (`keyname`,`value`) values ({keyname},{value}) on duplicate key update `value`=values(`value`)',
+                    [
+                        'keyname'=>'amazon_version',
+                        'value'=>$_SESSION['amazon_version']
+                    ]);
+                    $db->direct('insert into amazon_seller_partner_api (`keyname`,`value`) values ({keyname},{value}) on duplicate key update `value`=values(`value`)',
+                    [
+                        'keyname'=>'amazon_state',
+                        'value'=>$_SESSION['amazon_state']
+                    ]);
+                    $db->direct('insert into amazon_seller_partner_api (`keyname`,`value`) values ({keyname},{value}) on duplicate key update `value`=values(`value`)',
+                    [
+                        'keyname'=>'amazon_callback_uri',
+                        'value'=>$_SESSION['amazon_callback_uri']
+                    ]);
+                    $db->direct('insert into amazon_seller_partner_api (`keyname`,`value`) values ({keyname},{value}) on duplicate key update `value`=values(`value`)',
+                    [
+                        'keyname'=>'selling_partner_id',
+                        'value'=>$_SESSION['selling_partner_id']
+                    ]);
+                    $db->direct('insert into amazon_seller_partner_api (`keyname`,`value`) values ({keyname},{value}) on duplicate key update `value`=values(`value`)',
+                    [
+                        'keyname'=>'tualo_state_for_amazon',
+                        'value'=>$_SESSION['tualo_state_for_amazon']
+                    ]);
+
+                    unset($_SESSION['amazon_state']);
+                    unset($_SESSION['amazon_version']);
+                    unset($_SESSION['selling_partner_id']);
                     unset($_SESSION['amazon_callback_uri']);
                     session_commit();
                     header('Location: '.$url);
--- src/Routes/Route.php
+++ src/Routes/Route.php
@@ -9,6 +9,17 @@ class Route implements IRoute{
         BasicRoute::add('/amz-sp-api/oauth',function($matches){
             App::result('msg', 'ok' );
             App::result('success', true );
+            $db = App::get('session')->getDB();
+
+            foreach($_REQUEST as $key => $value ){
+                $db->direct('insert into amazon_seller_partner_api (`keyname`,`value`) values ({keyname},{value}) on duplicate key update `value`=values(`value`)',
+                [
+                    'keyname'=>$key,
+                    'value'=>$value
+                ]);
+            }
+            
+
             file_put_contents(dirname(App::get('tempPath')).'/info.json',json_encode($_REQUEST));
             App::contenttype('application/json');
         },['get','post'],false);
