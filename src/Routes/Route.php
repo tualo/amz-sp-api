fe526fcfbdcf659b80e8c371e5315688269b74e9
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2023-02-17 08:56:59 +0100

    initial

---
+++ src/Routes/Route.php
@@ -0,0 +1,18 @@
+<?php
+namespace Tualo\Office\AmanzonSellerPartner\Routes;
+use Tualo\Office\Basic\TualoApplication as App;
+use Tualo\Office\Basic\Route as BasicRoute;
+use Tualo\Office\Basic\IRoute;
+
+class Route implements IRoute{
+    public static function register(){
+        BasicRoute::add('/amz-sp-api/oauth',function($matches){
+            App::result('msg', 'hello world' );
+            App::result('success', true );
+            file_put_contents(App::get('tempPath').'/info.json',json_encode($_REQUEST));
+            App::contenttype('application/json');
+        },['get','post'],false);
+
+
+    }
+}
