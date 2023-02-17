840905402dc4a7584666566e05e43dc7cbdc4e21
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2023-02-17 09:05:44 +0100

    fix path

--- src/Routes/Route.php
+++ src/Routes/Route.php
@@ -7,9 +7,9 @@ use Tualo\Office\Basic\IRoute;
 class Route implements IRoute{
     public static function register(){
         BasicRoute::add('/amz-sp-api/oauth',function($matches){
-            App::result('msg', App::get('tempPath').'/info.json' );
+            App::result('msg', 'ok' );
             App::result('success', true );
-            file_put_contents(App::get('tempPath').'/info.json',json_encode($_REQUEST));
+            file_put_contents(dirname(App::get('tempPath')).'/info.json',json_encode($_REQUEST));
             App::contenttype('application/json');
         },['get','post'],false);
 
