090ce74182b49870daf2a787ddf4ffc1ee758bc7
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2023-02-20 07:39:45 +0100

    test refresh token

--- src/Routes/RefreshToken.php
+++ src/Routes/RefreshToken.php
@@ -21,9 +21,22 @@ class RefreshToken implements IRoute{
     public static function register(){
         BasicRoute::add('/amz-sp-api/refresh-token',function($matches){
             $db = App::get('session')->getDB();
-            $config = (App::get('configuration'))['amazon'] + $db->direct('select `keyname`,`value` from amazon_seller_partner_api',[],'keyname');
+            $config = (App::get('configuration'))['amazon'] + $db->directMap('select `keyname`,`value` from amazon_seller_partner_api',[],'keyname','value');
 
             App::result('success', $config['amazon'] );
+
+            $amazon_config = [
+                'lwaClientId' => $config['AMZ_CLIENT_ID'],
+                'lwaClientSecret' => $config['AMZ_CLIENT_SECRET'],
+                'lwaRefreshToken' => '',
+                'awsAccessKeyId' => $config['awsAccessKeyId'],
+                'awsSecretAccessKey' => $config['awsSecretAccessKey'],
+                'endpoint' => Endpoint::EU,
+            ];
+
+            $auth = SellingPartnerApi::Authentication($amazon_config); 
+            App::result('auth',$auth->requestLWAToken());
+
             App::contenttype('application/json');
         },['get','post'],false);
     }
